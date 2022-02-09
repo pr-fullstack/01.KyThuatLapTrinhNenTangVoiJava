f870a98dfecb84331d16cc32bd40af21749b1300
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-09 22:26:17 +0700

    [QuangNN] 4.Các kiểu dữ liệu, khai báo biến, lệnh gán

---
+++ 04-cac-kieu-du-lieu-khai-bao-bien-lenh-gan/src/basiclearning/KieuDuLieu.java
@@ -0,0 +1,13 @@
+package basiclearning;
+
+public class KieuDuLieu {
+    public static void main(String[] args) {
+        int b = 5;
+        int a = b;
+
+        System.out.println(a);
+    }
+
+
+
+}
