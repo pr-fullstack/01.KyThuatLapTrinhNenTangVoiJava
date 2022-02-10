36d84e8d67862ac2fd91dbbcdfffd98f7a8e95c9
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-10 15:06:36 +0700

    [QuangNN] 7.In thông tin ra màn hình console

---
+++ 07-in-thong-tin-ra-man-hinh-console/src/basiclearning/InThongTin.java
@@ -0,0 +1,14 @@
+package basiclearning;
+
+public class InThongTin {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+
+        // 2 + 3 = 5
+        System.out.println("(" + a + " + " + b + ")*2 = " + (a + b)*2);
+        System.out.printf("(%d + %d)*2 = %d", a, b, (a + b)*2);
+        // "a + b = 23"
+
+//        System.out.printf("%c - %d - %f - %s", 'A', 2054545, 20, "Ngọc Quang");
+    }
+}
