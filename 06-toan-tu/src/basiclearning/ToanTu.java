bddec6cafcedde61749053a1d6ea8b14720c6062
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-10 10:22:56 +0700

    [QuangNN] 6.Toán tử

---
+++ 06-toan-tu/src/basiclearning/ToanTu.java
@@ -0,0 +1,13 @@
+package basiclearning;
+
+public class ToanTu {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = 2;
+
+        System.out.println(a + a / (b * c));
+        // a / (b * c) = 4
+        // (a * b) / c = 1
+    }
+}
