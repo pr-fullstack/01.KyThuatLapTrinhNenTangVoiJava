71b84fc8655e0cd7efbc7ba4e02f2d492a127c92
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-11 15:07:30 +0700

    [QuangNN] 9. Bài tập tổng hợp 1 (Bài 5)

---
+++ 09-bai-tap-tong-hop-1/src/basiclearning/Bai5.java
@@ -0,0 +1,26 @@
+package basiclearning;
+
+import java.util.Scanner;
+
+/**
+ * 5.	Nhập bán kính của đường tròn.
+ *          Tính chu vi và diện tích của hình tròn đó.
+ *
+ *          Chu vi: c = 2 * r * pi;
+ *  		Diện tích: s = r * r * pi
+ */
+public class Bai5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập vào bán kính r: ");
+        double r = scanner.nextDouble();
+
+        double c = 2 * r * Math.PI;
+        double s = r * r * Math.PI;
+
+        System.out.println("Chu vi: " + c);
+        System.out.println("Diện tích: " + s);
+
+    }
+}
