0a2d65225b0161ef3776a64f113c2da8a746faa1
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-12 10:59:36 +0700

    [QuangNN] 10. Câu lệnh điều kiện, rẻ nhánh, toán tử 3 ngôi (Câu lệnh điều kiện)

---
+++ 10-cau-lenh-dieu-kien-re-nhanh-toan-tu-3-ngoi/src/basiclearning/CauLenhDieuKien.java
@@ -0,0 +1,11 @@
+package basiclearning;
+
+public class CauLenhDieuKien {
+    public static void main(String[] args) {
+        int x = 2;
+
+        if(x == 3); {
+            System.out.println("Rơi vào if");
+        }
+    }
+}
