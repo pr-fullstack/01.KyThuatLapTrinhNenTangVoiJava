2880ecadb4cceb9f72924b5f53cd49c23556d55f
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-12 22:20:59 +0700

    [QuangNN] 10. Câu lệnh điều kiện, rẻ nhánh, toán tử 3 ngôi (Giải phương trình bậc 2)

---
+++ 10-cau-lenh-dieu-kien-re-nhanh-toan-tu-3-ngoi/src/basiclearning/GiaiPhuongTrinhBacHai.java
@@ -0,0 +1,50 @@
+package basiclearning;
+
+import java.util.Scanner;
+
+/**
+ * Giải phương trình bậc 2 ax2+bx+c=0
+ * <p>
+ * Ctrl + Alt + L => Format code
+ */
+public class GiaiPhuongTrinhBacHai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập vào a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Nhập vào b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Nhập vào c: ");
+        double c = scanner.nextDouble();
+
+        if (a == 0) { // Phương trình bậc nhất
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình vô số nghiệm");
+                } else { //c != 0
+                    System.out.println("Phương trình vô nghiệm");
+                }
+            } else { // b != 0
+                System.out.println("Phương trình có nghiệm x = " + -c / b);
+            }
+        } else { // Phương trình bậc 2 có a != 0
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            }
+
+            if (delta == 0) {
+                System.out.println("Phương trình có nghiệm duy nhất : x = " + -b / (2 * a));
+            }
+
+            if (delta > 0) {
+                System.out.println("x1 = " + (-b + Math.sqrt(delta)) / (2 * a));
+                System.out.println("x2 = " + (-b - Math.sqrt(delta)) / (2 * a));
+            }
+        }
+    }
+}
