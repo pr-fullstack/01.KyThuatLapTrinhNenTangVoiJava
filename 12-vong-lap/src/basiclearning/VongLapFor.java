bc49b67c37e9f9fb46fb651f2f195d13874f8ae8
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-14 17:26:47 +0700

    [QuangNN] 12. Vòng lặp (Vòng lặp For)

---
+++ 12-vong-lap/src/basiclearning/VongLapFor.java
@@ -0,0 +1,16 @@
+package basiclearning;
+
+public class VongLapFor {
+    public static void main(String[] args) {
+        int i = 0;
+        for (; i < 5; i++) {
+            System.out.println(i);
+        }
+        // Bước 1: Khởi tạo i = 0 => thỏa mãn => Xuất 0
+        // Bước 2: i = 1 => thỏa mãn => Xuất 1
+        // Bước 3: i = 2 => thỏa mãn => Xuất 2
+        // Bước 4: i = 3 => thỏa mãn => Xuất 3
+        // Bước 5: i = 4 => thỏa mãn => Xuất 4
+        // Bước 6: i = 5 => không thỏa mãn =>
+    }
+}
