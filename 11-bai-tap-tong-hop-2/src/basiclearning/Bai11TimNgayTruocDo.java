19dcfd9fdd9f336a72b71005c678aa67d4555358
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-14 13:41:17 +0700

    [QuangNN] 11. Bài tập tổng hợp 2 (Bài 11 - Tìm ngày trước đó)

---
+++ 11-bai-tap-tong-hop-2/src/basiclearning/Bai11TimNgayTruocDo.java
@@ -0,0 +1,77 @@
+package basiclearning;
+
+import java.util.Scanner;
+
+/**
+ * 11.	(**) Nhập vào thông tin 1 ngày (ngày - tháng - năm).
+ * Kiểm tra ngày có hợp lệ hay không ?
+ * Nếu hợp lệ hãy tìm ra ngày kế tiếp (ngày - tháng - năm)
+ * & ngày trước đó (ngày - tháng - năm).
+ */
+public class Bai11TimNgayTruocDo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập vào ngày: ");
+        int day = scanner.nextInt();
+
+        System.out.print("Nhập vào tháng: ");
+        int month = scanner.nextInt();
+
+        System.out.print("Nhập vào năm: ");
+        int year = scanner.nextInt();
+
+        int maxDayOfMonth;
+        boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+        switch (month) {
+            case 2:
+                if (isLeapYear) {
+                    maxDayOfMonth = 29;
+                } else {
+                    maxDayOfMonth = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                maxDayOfMonth = 30;
+                break;
+            default:
+                maxDayOfMonth = 31;
+        }
+
+        if (year < 0 || month < 0 || month > 12 || day < 0 || day > maxDayOfMonth) {
+            System.out.printf("%d-%d-%d không phải là ngày hợp lệ", year, month, day);
+        } else {
+            day--;
+            if (day == 0) {
+                month--;
+                if (month == 0) {
+                    month = 12;
+                    year--;
+                }
+                switch (month) {
+                    case 2:
+                        if (isLeapYear) {
+                            day = 29;
+                        } else {
+                            day = 28;
+                        }
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        day = 30;
+                        break;
+                    default:
+                        day = 31;
+                }
+
+                System.out.printf("%d-%d-%d là ngày trước đó", year, month, day);
+            }
+        }
+    }
+}
