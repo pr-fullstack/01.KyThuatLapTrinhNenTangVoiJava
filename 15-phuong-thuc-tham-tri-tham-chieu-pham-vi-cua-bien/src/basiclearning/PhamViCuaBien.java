f1d7d25d67117aa709280c3571d5561e8057caca
Ngoc Quang <ngocquang12395@gmail.com>
2022-02-17 20:48:03 +0700

    [QuangNN] 15. Phương thức, tham trị & tham chiếu, phạm vi của biến. (Phạm vi của biến)

---
+++ 15-phuong-thuc-tham-tri-tham-chieu-pham-vi-cua-bien/src/basiclearning/PhamViCuaBien.java
@@ -0,0 +1,15 @@
+package basiclearning;
+
+public class PhamViCuaBien {
+    static int a = 100;
+    static int b = 100;
+
+    static void method(int a) {
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void main(String[] args) {
+        method(5);
+    }
+}
