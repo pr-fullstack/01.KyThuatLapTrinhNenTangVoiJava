f5e776a474c3da0f9171bcb26bf3c8a918cea9c0
DESKTOP-80AV1OO\NGOCQUANG <ngocquang12395@gmail.com>
2022-02-22 09:31:22 +0700

    [QuangNN] 18. String (Chuỗi)

---
+++ 18-string-chuoi/src/basiclearning/StringChuoi.java
@@ -0,0 +1,9 @@
+package basiclearning;
+
+public class StringChuoi {
+    public static void main(String[] args) {
+        String str = "Nguyễn Ngọc Quang";
+        System.out.println(str.substring(7)); // "Ngọc Quang"
+        System.out.println(str.substring(7, 11)); // "Ngọc"
+    }
+}
