7acfafce96d9d499538b0005f8a1f6892ad129d9
ngocquang95 <ngocquang12395@gmail.com>
2022-02-28 23:44:40 +0700

    [QuangNN] 25. Ép kiểu cho kiểu dữ liệu nguyên thủy

---
+++ 25-ep-kieu-cho-kieu-du-lieu-nguyen-thuy/src/basiclearning/EpKieuChoKieuDuLieuNguyenThuy.java
@@ -0,0 +1,10 @@
+package basiclearning;
+
+public class EpKieuChoKieuDuLieuNguyenThuy {
+    public static void main(String[] args) {
+        short a = 1000;
+        byte b = (byte) a;
+
+        System.out.println(b);
+    }
+}
