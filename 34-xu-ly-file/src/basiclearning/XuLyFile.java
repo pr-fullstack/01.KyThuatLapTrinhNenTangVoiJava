a14892a1b8fff83449dc64195d3f67d4ec826c71
ngocquang95 <ngocquang12395@gmail.com>
2022-03-19 17:09:46 +0700

    [QuangNN] 34. Xử lý file (Đọc file)

---
+++ 34-xu-ly-file/src/basiclearning/XuLyFile.java
@@ -0,0 +1,20 @@
+package basiclearning;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class XuLyFile {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(Paths.get("34-xu-ly-file/src/resources/input.txt"));
+
+
+        String str = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+        System.out.println(str);
+        System.out.println(str2);
+
+        scanner.close();
+    }
+}
