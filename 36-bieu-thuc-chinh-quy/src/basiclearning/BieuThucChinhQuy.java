cfc6800de053ef7a808ce0d82c64a990677de85e
ngocquang95 <ngocquang12395@gmail.com>
2022-03-20 11:22:55 +0700

    [QuangNN] 36. Biểu thức chính quy

---
+++ 36-bieu-thuc-chinh-quy/src/basiclearning/BieuThucChinhQuy.java
@@ -0,0 +1,9 @@
+package basiclearning;
+
+public class BieuThucChinhQuy {
+    public static void main(String[] args) {
+        String regex = "\\D\\d";
+
+        System.out.println("A4".matches(regex));
+    }
+}
